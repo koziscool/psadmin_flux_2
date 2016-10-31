eb9cd30cc1cd2304e49477ad53f11099a78c58bf
koziscool <kozjob@yahoo.com>
2016-10-30 19:28:51 -0500

    first cut routing

--- src/main.js
+++ src/main.js
@@ -3,7 +3,29 @@
 $ = jQuery = require('jquery');
 var React = require('react');
 var Home = require('./components/homePage');
+var About = require('./components/about/aboutPage');
 
-React.render( <Home/>, document.getElementById('app'));
+var App = React.createClass({
+    render: function () {
+        var Child;
 
+        switch(this.props.route){
+            case 'about': Child = About; break;
+            default: Child = Home;
+        }
 
+        return (
+            <div>
+                <Child/>
+            </div>
+        );
+    }
+});
+
+var render = function() {
+    var route = window.location.hash.substr(1);
+    React.render( <App route={route}/>, document.getElementById('app') );
+};
+
+window.addEventListener('hashchange', render);
+render();
